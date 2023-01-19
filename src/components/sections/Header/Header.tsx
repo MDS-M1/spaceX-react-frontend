d38f0a037832969d58dc88b67c983f7c11f6b839
Rémi Rubis <remi.rubis@yahoo.com>
2023-01-19 10:25:55 +0100

    feat(layout/default): create default layout to setup page template

--- src/components/sections/Header/Header.tsx
+++ src/components/sections/Header/Header.tsx
@@ -1,7 +1,20 @@
+import { Link } from 'react-router-dom';
 import './Header.scss';
 
+import logo from 'assets/img/logo.svg';
+
 const Header = () => {
-  return <p>It's an Header</p>;
+  return (
+    <header>
+      <Link to="">Launches</Link>
+      <a href="/">
+        <figure>
+          <img src={logo} alt="SpaceX logo" />
+        </figure>
+      </a>
+      <Link to="">Capsules</Link>
+    </header>
+  );
 };
 
 export default Header;
